d1b948489a9900f7056751f49711f1e6e7d0e0a7
borgssam <borgssam@gmail.com>
2024-07-21 23:06:29 +0845

    p63

---
+++ src/step01/App.js
@@ -0,0 +1,10 @@
+import {Potato} from "./Potato"
+function App() {
+  return (
+    <div >
+      <Potato/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/step01/Potato.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+export function Potato() { return <h3>I love potato</h3>;}
+// export default Potato;
